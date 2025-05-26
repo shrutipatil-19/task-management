fefea1d8394149432fc679bc8738ea007baa3abf
shrutipatil-19 <shrupatil1319@gmail.com>
2025-05-26 17:26:31 +0530

    remove route

--- resources/views/admin/partials/Sidebar.blade.php
+++ resources/views/admin/partials/Sidebar.blade.php
@@ -47,7 +47,7 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a href="{{ route('tasks.index') }}" class="nav-link">
+                <a href="" class="nav-link">
                     <i class="link-icon" data-feather="message-square"></i>
                     <span class="link-title">Task</span>
                 </a>
