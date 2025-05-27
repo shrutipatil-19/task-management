d705f9a3509607d20e2e5a6251a0cb6b19ec4b79
shrutipatil-19 <shrupatil1319@gmail.com>
2025-05-27 14:19:25 +0530

    database change

--- database/migrations/2025_05_26_060947_create_tasks_table.php
+++ database/migrations/2025_05_26_060947_create_tasks_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
-            $table->unsignedBigInteger('project');
-            $table->unsignedBigInteger('assigned_to');
+            $table->string('project');
+            $table->string('assigned_to');
             $table->enum('priority', ['low', 'medium', 'high', 'urgent'])->default('medium');
             $table->enum('status', ['to_do', 'in_progress', 'review', 'done'])->default('to_do');
             $table->date('due_date')->nullable();
