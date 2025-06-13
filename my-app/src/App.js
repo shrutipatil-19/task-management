13a01ef7b3b0e1fb6771772f91d5253c03b2a37c
shrutipatil-19 <shrupatil1319@gmail.com>
2025-06-13 18:54:46 +0530

    display view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [msg, setMsg] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:8000/api/hello')
+    fetch('http://localhost:8000/task-management/api/hello')
       .then(res => res.json())
       .then(data => setMsg(data.message));
   }, []);
