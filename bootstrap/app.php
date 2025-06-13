eccecca6f51cf25adbd071667113c23923638047
shrutipatil-19 <shrupatil1319@gmail.com>
2025-06-13 18:47:13 +0530

    api registation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         commands: __DIR__ . '/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
         health: '/up',
     )
 
